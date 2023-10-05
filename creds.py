ba76de04ce75e6f4c1a416e6fac28014bcaeed15
Giacomo Tobiolo <giacomo.tobiolo@WPPIT-5111.local>
2023-10-05 16:51:58 +0100

    updated

--- creds.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host = "localhost",
-  user = "test1",
-  password = "Testingthistoseeifitworks",
-  database = "library",
-  buffered = True
-)
